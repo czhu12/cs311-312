970983dea9b00fb9ee8d319d1988ca3e06c9f0d3
Chris Zhu <chris_zhu12@hotmail.com>
2014-10-15 02:13:01 -0700

    tester is working

--- cs312/a1/rush.hs
+++ cs312/a1/rush.hs
@@ -49,7 +49,7 @@ swap board pos1 pos2 = secondBoard
   where 
     firstLetter = letterAtPosition (fst pos1) (snd pos1) board
     secondLetter = letterAtPosition (fst pos2) (snd pos2) board
-    firstBoard = replaceLetter pos1 firstLetter board 
+    firstBoard = replaceLetter pos2 firstLetter board 
     secondBoard = replaceLetter pos1 secondLetter firstBoard 
 -- firstPos means that the rest of the car must be further down or further right
 -- To move left, we should check if the place immediately before the firstPos is empty, if so
@@ -104,7 +104,7 @@ firstLetterPos' letter board pos
   | otherwise                                                   = firstLetterPos' letter board (nextPos pos)
 
 nextPos pos board
-  | x >= length (head board)        = (0, y + 1)
+  | x >= length (head board) -1     = (0, y + 1)
   | otherwise                       = (x + 1, y)
   where 
     x = fst pos
@@ -112,13 +112,15 @@ nextPos pos board
 
 letterAtPosition x y board = board !! y !! x
 
-isOutOfBounds x y board = or ((length board) >= y) ((length (board!!0)) >= x)
+isOutOfBounds x y board = ((length board) <= y) || ((length (board!!0)) <= x)
 
+replaceNth :: Int -> Char -> [Char] -> [Char]
 replaceNth n newVal (x:xs)
      | n == 0 = newVal:xs
      | otherwise = x:replaceNth (n-1) newVal xs
 
+replaceLetter :: (Int, Int) -> Char -> [[Char]] -> [[Char]]
 replaceLetter at withLetter board 
-  | (snd at) == 0         = replaceNth (fst at) withLetter (head board):(tail board)
-  | otherwise             = (head board):(replaceLetter at withLetter (tail board))
+  | (snd at) == 0         = (replaceNth (fst at) withLetter (head board)):(tail board)
+  | otherwise             = (head board):(replaceLetter ((fst at), ((snd at) - 1)) withLetter (tail board))
 
--- cs312/a1/tester.hs
+++ cs312/a1/tester.hs
@@ -1,19 +1,23 @@
 b = ["--A", "--A", "---"]
+c = ["---", "-AA", "---"]
+
 swap board pos1 pos2 = secondBoard
   where 
     firstLetter = letterAtPosition (fst pos1) (snd pos1) board
     secondLetter = letterAtPosition (fst pos2) (snd pos2) board
-    firstBoard = replaceLetter pos1 firstLetter board 
+    firstBoard = replaceLetter pos2 firstLetter board 
     secondBoard = replaceLetter pos1 secondLetter firstBoard 
 
 letterAtPosition x y board = board !! y !! x
 
+replaceLetter :: (Int, Int) -> Char -> [[Char]] -> [[Char]]
 replaceLetter at withLetter board 
-  | (snd at) == 0         = replaceNth (fst at) withLetter (head board):(tail board)
-  | otherwise             = (head board):(replaceLetter at withLetter (tail board))
+  | (snd at) == 0         = (replaceNth (fst at) withLetter (head board)):(tail board)
+  | otherwise             = (head board):(replaceLetter ((fst at), ((snd at) - 1)) withLetter (tail board))
 
+replaceNth :: Int -> Char -> [Char] -> [Char]
 replaceNth n newVal (x:xs)
-     | n == 0 = newVal:xs
+     | n == 0    = newVal:xs
      | otherwise = x:replaceNth (n-1) newVal xs
 
 isVertical letter board
@@ -21,7 +25,7 @@ isVertical letter board
   | (letterAtPosition x (y - 1) board) == letter        = True
   | (letterAtPosition (x + 1) y board) == letter        = False
   | (letterAtPosition (x - 1) y board) == letter        = False
-  | otherwise                                   = error "isVertical: can't find vertical or horizontal"
+  | otherwise                                           = error "isVertical: can't find vertical or horizontal"
   where 
     firstPos = firstLetterPos letter board
     x        = fst firstPos
@@ -37,10 +41,10 @@ firstLetterPos' letter board pos
   | otherwise                                                   = firstLetterPos' letter board (nextPos pos board)
 
 nextPos pos board
-  | x >= length (head board)        = (0, y + 1)
+  | x >= length (head board) -1     = (0, y + 1)
   | otherwise                       = (x + 1, y)
   where 
     x = fst pos
     y = snd pos
 
-isOutOfBounds x y board = ((length board) >= y) || ((length (board!!0)) >= x)
+isOutOfBounds x y board = ((length board) <= y) || ((length (board!!0)) <= x)
